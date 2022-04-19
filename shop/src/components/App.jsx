93eb51f47b985d06f61da747c6d6091123dcdb42
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-04-19 15:27:36 +0300

    key

--- shop/src/components/App.jsx
+++ shop/src/components/App.jsx
@@ -1,7 +1,19 @@
 import { useQuery } from '@apollo/client';
 import { GET_CATEGORIES } from 'query/categories';
+import { useEffect, useState } from 'react';
 export const App = () => {
   const { loading, error, data } = useQuery(GET_CATEGORIES);
-  console.log(data);
-  return <div>React homework template</div>;
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    if (!loading) {
+      setCategories(data.categories);
+    }
+  }, [data, loading]);
+  return (
+    <div>
+      {categories.map((categori, index) => (
+        <div key={index}> {categori.name}</div>
+      ))}
+    </div>
+  );
 };
