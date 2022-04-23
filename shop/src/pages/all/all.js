80cc9d65ceee129c24aeae97e41d867ce1d6ebdb
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-04-24 00:56:16 +0300

    new function

--- shop/src/pages/all/all.js
+++ shop/src/pages/all/all.js
@@ -2,26 +2,43 @@ import { Query } from '@apollo/client/react/components';
 import { GET_CATEGORIES } from 'query/categories';
 import { Component } from 'react';
 class All extends Component {
-  getCurrence(arr) {
-    console.log(arr, this.props.currencie);
-    return arr[0].currency.label;
-  }
+  // getCurrence(arr) {
+  //   console.log(arr, this.props.currencie);
+  //   return arr[0].currency.label;
+  // }
+  state = {
+    cartVisible: false,
+  };
+
+  onCartVisible = e => {
+    console.log(e.target);
+    this.setState({ cartVisible: true });
+  };
+  onCartInvisible = () => {
+    this.setState({ cartVisible: false });
+  };
 
   render() {
+    const { cartVisible } = this.state;
     const { currencie } = this.props;
 
     return (
       <div>
         <Query query={GET_CATEGORIES}>
           {({ loading, data }) => {
-            console.log(data);
             if (loading) return 'Loading...';
 
             const { categories } = data;
             return categories.map(category => {
               if (category.name === 'all') {
-                return category.products.map(product => (
-                  <div key={product.id}>
+                return category.products.map((product, index) => (
+                  <div
+                    key={product.id}
+                    onMouseOver={this.onCartVisible}
+                    onMouseOut={this.onCartInvisible}
+                    style={{ display: 'inline-block' }}
+                    value={index}
+                  >
                     <img
                       src={product.gallery[0]}
                       alt={product.name}
@@ -33,10 +50,13 @@ class All extends Component {
                       {product.prices
                         .filter(price => price.currency.label === currencie)
                         .map(cost => (
-                          <span key={cost.label}>{cost.amount}</span>
+                          <span key={cost.currency.label}>
+                            {cost.amount}
+                            {cost.currency.symbol}
+                          </span>
                         ))}
-                      {currencie}
                     </p>
+                    {cartVisible && <p>cart</p>}
                   </div>
                 ));
               }
